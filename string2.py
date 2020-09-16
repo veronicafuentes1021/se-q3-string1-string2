a70a214fd732bbffc4d81322dc85382995fb23be
Veronica Fuentes <veronica.elsa1021@gmail.com>
2020-09-15 23:31:47 -0500

    verbing function is working

--- string2.py
+++ string2.py
@@ -4,7 +4,7 @@ Kenzie assignment: String2
 """
 # Your name, plus anyone who helped you with this assignment.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Veronica Fuentes"
 
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
@@ -23,8 +23,12 @@ __author__ = "???"
 
 
 def verbing(s):
-    # your code here
-    return
+    if len(s) < 3 :
+        return s
+    elif s.endswith("ing"):
+        return s + "ly"
+    else:
+        return s + "ing"
 
 
 # E. not_bad
